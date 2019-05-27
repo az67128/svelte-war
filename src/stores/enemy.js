bd539a0c82062d05b80187693887885c6f2f59f5
Alexander Zinchenko <az67128@gmail.com>
2019-05-27 23:36:40 +0300

    bundle update

--- src/stores/enemy.js
+++ src/stores/enemy.js
@@ -1,6 +1,4 @@
 import { writable } from 'svelte/store';
 
-// Массив врагов
 export const enemyList = writable([]);
-// Временная метка добавления последнего врага
 export const lastEnemyAddedAt = writable(0);
